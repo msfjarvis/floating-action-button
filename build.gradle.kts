52d3c1526e9460f468a276d691b053553e23380c
Harsh Shandilya <msfjarvis@gmail.com>
2019-05-25 13:24:11 +0530

    Dependency updates
    AGP 3.4.1
    Kotlin 1.3.31
    Gradle 5.4.1
    
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ buildscript {
         jcenter()
     }
     dependencies {
-        classpath("com.android.tools.build:gradle:3.3.2")
-        classpath(kotlin("gradle-plugin", "1.3.30"))
+        classpath("com.android.tools.build:gradle:3.4.1")
+        classpath(kotlin("gradle-plugin", "1.3.31"))
         classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
     }
 }
@@ -18,6 +18,11 @@ allprojects {
 }
 
 tasks {
+    named<Wrapper>("wrapper") {
+        gradleVersion = "5.4.1"
+        distributionType = Wrapper.DistributionType.ALL
+    }
+
     register("clean", Delete::class) {
         delete(rootProject.buildDir)
     }
