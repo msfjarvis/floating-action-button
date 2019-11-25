c40e381744f5e2e01fcbf7543a23c702cc599562
Harsh Shandilya <msfjarvis@gmail.com>
2019-11-25 19:55:32 +0530

    Init sample
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>

---
+++ sample/src/main/java/me/msfjarvis/floatingactionbutton/sample/MainActivity.kt
@@ -0,0 +1,18 @@
+/*
+ * Copyright © 2014 Jerzy Chalupski
+ * Copyright © 2018 Jason A. Donenfeld <Jason@zx2c4.com>
+ * Copyright © 2018-2019 Harsh Shandilya <msfjarvis@gmail.com> All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0
+ */
+package me.msfjarvis.floatingactionbutton.sample
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
